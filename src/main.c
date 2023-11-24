3d5fb3c7b5914e4ac40f92855240205bd416e02a
Elisa Rivero Perez <erivero-@c3r9s6.42malaga.com>
2023-11-24 16:31:44 +0100

    init structs

--- src/main.c
+++ src/main.c
@@ -4,6 +4,14 @@ void	ft_leaks(void)
 {
 	system("leaks -q cub3D");
 }
+void ft_init_structs(t_game *info)
+{
+	info->map = malloc(sizeof(t_map *));
+	if (!info->map) //con los malloc y toa la pesca
+		return ;
+	info->map->test = "socorro";
+//	info->imgs (...)
+}
 
 int	main(int ac, char **av)
 {
@@ -12,6 +20,7 @@ int	main(int ac, char **av)
 	t_game	info;
 
 	fd = ft_arg_check(ac, av);
+	ft_init_structs(&info);
 	if (fd < 0)
 		return (-1);
 	ft_parse(&info, fd); //-> carga del mapa en memoria y checkeo de que esté correcto
