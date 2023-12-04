0f7a1e1fb81cd394faaa41913eb80bdb4f1b00eb
Elisa Rivero Perez <erivero-@c3r9s6.42malaga.com>
2023-12-04 11:35:24 +0100

    rgb to hexa

---
+++ src/parser/colour_handle.c
@@ -0,0 +1,84 @@
+#include "../../inc/cub3D.h"
+
+static int	ft_char_counter(char *str, char c)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			count++;
+		i++;
+	}
+	return (count);
+}
+
+static int	ph_atoi(char *str)
+{
+	int	i;
+	int	num;
+
+	i = 0;
+	num = 0;
+	if (str[i] == '+')
+		i++;
+	else if (str[i] == '-') //si es negativo
+		return (-1);
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		if (!ft_isdigit(str[i]))
+			return (-1);
+		num = num * 10 + str[i] - 48;
+		i++;
+	}
+	return (num);
+
+}
+
+static int	ft_rgb_to_hex(int r, int g, int b)
+{
+	return ((r << 16) | (g << 8) | b);
+}
+/* toma los 3 enteros y los va desplazando a la izquierda para que el rojo ocupe la primera
+posición, seguido del verde y del azul, y combinándolos con or en principio las funciones
+de mlx deberían ser capaces de leerlo tal cual (o eso me gusta pensar xd)*/
+
+
+int	ft_get_colour(int *rgb)
+{
+	int	r;
+	int	g;
+	int	b;
+
+	r = ph_atoi(rgb[0]);
+	g = ph_atoi(rgb[1]);
+	b = ph_atoi(rgb[2]);
+
+	if (r < 0 || r > 255 || g < 0 || g > 255
+		|| b < 0 || b > 255)
+		return (-1);
+	return (ft_rgb_to_hex(r, g, b));
+}
+int	ft_color_check(char *color)
+{
+	int		i;
+	int		cont;
+	int		ret;
+	char	**rgb;
+
+	ret = 0;
+	i = -1;
+
+	if (ft_char_counter(color, ',') > 2)
+		return (-1);
+	rgb = ft_split(color, ',');
+	if (ft_arrlen(rgb) != 3)
+		ret = -1;
+	ret = ft_get_colour(rgb);
+	ft_free_double_pointer(rgb);
+//	ft_printf("ret is: %i", ret);
+	return (ret);
+}
--- src/parser/scene_check.c
+++ src/parser/scene_check.c
@@ -13,75 +13,14 @@ static int	ft_texture_check(t_scene *scene)
 	return (0);
 }
 
-static int	ph_atoi(char *str)
-{
-	int	i;
-	int	num;
-
-	i = 0;
-	num = 0;
-	if (str[i] == '+')
-		i++;
-	else if (str[i] == '-') //si es negativo
-		return (-1);
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		if (!ft_isdigit(str[i]))
-			return (-1);
-		num = num * 10 + str[i] - 48;
-		i++;
-	}
-	return (num);
-
-}
-static int	ft_char_counter(char *str, char c)
-{
-	int	i;
-	int	count;
-
-	i = 0;
-	count = 0;
-	while (str[i])
-	{
-		if (str[i] == c)
-			count++;
-		i++;
-	}
-	return (count);
-}
-
-int	ft_color_check(char *color)
-{
-	int		i;
-	int		cont;
-	int		ret;
-	char	**rgb;
-
-	ret = 0;
-	i = -1;
-
-	if (ft_char_counter(color, ',') > 2)
-		return (-1);
-	rgb = ft_split(color, ',');
-	if (ft_arrlen(rgb) != 3)
-		ret = -1;
-	if (ph_atoi(rgb[0]) < 0 || ph_atoi(rgb[0]) > 255)
-		ret = -1;
-	if (ph_atoi(rgb[1]) < 0 || ph_atoi(rgb[1]) > 255)
-		ret = -1;
-	if (ph_atoi(rgb[2]) < 0 || ph_atoi(rgb[2]) > 255)
-		ret = -1;
-	ft_free_double_pointer(rgb);
-//	ft_printf("ret is: %i", ret);
-	return (ret);
-}
-
 int	ft_scene_check(t_scene *scene)
 {
 	ft_print_scene(scene, DEBUG_COLOR);
 	if (ft_texture_check(scene) == -1)
 		return (ft_error(EXT_PNG, NULL));
-	if (ft_color_check(scene->f_color)  == -1 || ft_color_check(scene->c_color) == -1)
+	scene->floor = ft_color_check(scene->f_color); //no sé si guardar estos enteros en otra estructura
+	scene->ceiling = ft_color_check(scene->c_color);
+	if (scene->floor  == -1 || scene->ceiling == -1)
 		return (ft_error(COLOR, NULL));
 	if (ft_char_mapcheck(scene->map) == -1)
 		return (-1);
