00975f78a01a5d144ddb4e5094426bf61792d384
Elisa Rivero Perez <erivero-@c3r9s6.42malaga.com>
2023-12-15 13:06:14 +0100

    First steps from raycast to drawing

--- inc/cub3D.h
+++ inc/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: erivero- <erivero-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 10:52:50 by marirodr          #+#    #+#             */
-/*   Updated: 2023/12/14 10:21:58 by erivero-         ###   ########.fr       */
+/*   Updated: 2023/12/15 11:32:18 by erivero-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,6 +51,9 @@ int		ft_char_mapcheck(char **map);
 
 //ft_wall_checker.c
 int	ft_wall_check(char **map, t_scene *scene);
+
+/*							RAYCAST							*/
+double	ft_ray_caster(t_game *info, float angle);
 
 /*							UTILS							*/
 //error_handling.c							
--- src/raycast/3Der.c
+++ src/raycast/3Der.c
@@ -1,16 +1,36 @@
 #include "../../inc/cub3D.h"
 
-float 	ft_scalator_supremexd()
+float 	ft_scalator(t_game *info, float angle)
 {
 	float	scale;
-	float	distance;
+	float	len; //distancia al plano de proyección. Es fija
+	float	distance; //distancia real al rayo
+	float	deltang; //ángulo entre el rayo y el vector de dirección
 
-	distance = (WIDTH / 2) / tan(45); //45 hay que pasarlo a radianes pero bueno xd
-	scale = TILE / ft_raycaster() * distance;//el alto del tile está sin definir
+	len = (WIDTH / 2) / tan(ft_deg_to_rad(angle));
+	deltang = info->player->angle - angle;
+	distance = ft_raycaster(info, angle) * cos(ft_deg_to_rad(deltang));
+	scale = WALL_H / distance * len;
 	return (scale);
 }
 
-void	ft_fantastic_3Der(t_game *info)
+void	ft_draw_coll(t_game	*info, float scale, int	coll)
+{
+	int	j;
+	int	top; //el pixel de arriba del todo del muro
+	int	wall; //altura del muro en píxeles?
+
+	wall = WALL_H / scale; //digo yo que la pared mide en píxeles su altura entre la escala
+	top = HEIGHT / 2 - wall / 2;
+	while (j < HEIGHT)
+	{
+		//cuando j sea top habrá que empezar a pintar muro
+		//cuando j sea top + wall habrá que dejar de pintar muro
+		j++;
+	}
+}
+
+void	ft_3Der(t_game *info)
 {
 	float	angle;
 	float	a;
@@ -18,14 +38,15 @@ void	ft_fantastic_3Der(t_game *info)
 	int		i;
 	int		j;
 
-	angle = info->player->angle - 45; //empezamos a lanzar desde -45 y hasra +45
-	a = 1080 / 90; //lo que tengo que aumentar el ángulo en cada iteración
+	angle = info->player->angle - (FOV / 2); //empezamos a lanzar desde -45 y hasta +45
+	a = WIDTH / FOV; //lo que tengo que aumentar el ángulo en cada iteración
 	i = 0;
 	if (angle < 0)
 		angle += 360;
-	while (i < 1080)
+	while (i < WIDTH)
 	{
-		scale = ft_scalator_supremexd();
+		scale = ft_scalator(info, angle);
+		ft_draw_coll(info, scale, i); // (?)
 		i++;
 		angle += a;
 		if (angle > 359)
