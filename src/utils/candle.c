0700c3fcb303aef74b7c4fa0d3a0c1473e19c76b
Elisa Rivero Perez <erivero-@c3r10s6.42malaga.com>
2024-01-09 17:19:37 +0100

    relative path on candle

--- src/utils/candle.c
+++ src/utils/candle.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   candle.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marirodr <marirodr@student.42malaga.com>   +#+  +:+       +#+        */
+/*   By: erivero- Desktopro-@Desktopt.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/09 12:00:08 by marirodr          #+#    #+#             */
-/*   Updated: 2024/01/09 16:54:31 by marirodr         ###   ########.fr       */
+/*   Created: 2024/01/09 12:00:08 by erivero- Desktop   #+#    #+#             */
+/*   Updated: 2024/01/09 17:15:02 by erivero- Desktop  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,17 +38,17 @@ mlx_texture_t *ft_load_candle_text(int frame)
 
 	txt = NULL;
 	if (frame == 0)
-		txt = mlx_load_png("/Users/marirodr/cursus/cub3d/texturas/vela1.png");
+		txt = mlx_load_png("./texturas/vela1.png");
 	else if (frame == 1 || frame == -1)
-		txt = mlx_load_png("/Users/marirodr/cursus/cub3d/texturas/vela2.png");
+		txt = mlx_load_png("./texturas/vela2.png");
 	else if (frame == 2 || frame == -2)
-		txt = mlx_load_png("/Users/marirodr/cursus/cub3d/texturas/vela3.png");
+		txt = mlx_load_png("./texturas/vela3.png");
 	else if (frame == 3 || frame == -3)
-		txt = mlx_load_png("/Users/marirodr/cursus/cub3d/texturas/vela4.png");
+		txt = mlx_load_png("./texturas/vela4.png");
 	else if (frame == 4 || frame == -4)
-		txt = mlx_load_png("/Users/marirodr/cursus/cub3d/texturas/vela5.png");
+		txt = mlx_load_png("./texturas/vela5.png");
 	else if (frame == 5 || frame == -5)
-		txt = mlx_load_png("/Users/marirodr/cursus/cub3d/texturas/vela6.png");
+		txt = mlx_load_png("./texturas/vela6.png");
 	if (!txt)
 		printf("NO TEXTURA VELA\n");
 	return (txt);
@@ -56,22 +56,22 @@ mlx_texture_t *ft_load_candle_text(int frame)
 
 // int	ft_load_candle_text(t_game *game, t_img *img)
 // {
-// 	img->candle_text[0] = mlx_load_png("/Users/marirodr/cursus/cub3d/texturas/vela1.png");
+// 	img->candle_text[0] = mlx_load_png("./texturas/vela1.png");
 // 	if (!img->candle_text[0])
 // 		return (printf("NO TEXTURA DE VELA1\n"), 1);
-// 	img->candle_text[1] = mlx_load_png("/Users/marirodr/cursus/cub3d/texturas/vela2.png");
+// 	img->candle_text[1] = mlx_load_png("./texturas/vela2.png");
 // 	if (!img->candle_text[1])
 // 		return (printf("NO TEXTURA DE VELA2\n"), 1);
-// 	img->candle_text[2] = mlx_load_png("/Users/marirodr/cursus/cub3d/texturas/vela3.png");
+// 	img->candle_text[2] = mlx_load_png("./texturas/vela3.png");
 // 	if (!img->candle_text[2])
 // 		return (printf("NO TEXTURA DE VELA3\n"), 1);
-// 	img->candle_text[3] = mlx_load_png("/Users/marirodr/cursus/cub3d/texturas/vela4.png");
+// 	img->candle_text[3] = mlx_load_png("./texturas/vela4.png");
 // 	if (!img->candle_text[3])
 // 		return (printf("NO TEXTURA DE VELA4\n"), 1);
-// 	img->candle_text[4] = mlx_load_png("/Users/marirodr/cursus/cub3d/texturas/vela5.png");
+// 	img->candle_text[4] = mlx_load_png("./texturas/vela5.png");
 // 	if (!img->candle_text[4])
 // 		return (printf("NO TEXTURA DE VELA5\n"), 1);
-// 	img->candle_text[5] = mlx_load_png("/Users/marirodr/cursus/cub3d/texturas/vela6.png");
+// 	img->candle_text[5] = mlx_load_png("./texturas/vela6.png");
 // 	if (!img->candle_text[5])
 // 		return (printf("NO TEXTURA DE VELA6\n"), 1);
 // 	printf("HE CARGADO LAS TEXTURAS\n");
